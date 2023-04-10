41ab707a71f15e4bedda9b4ae818ea53e4ae9731
RasheedSk <rasheek.s@icon-tg.com>
2023-04-10 18:28:01 +0530

    removed salesforce

--- mvc/Models/solutionSalesforceModel.js
+++ mvc/Models/solutionSalesforceModel.js
@@ -1,22 +1,22 @@
-const mongoose = require('mongoose');
-const solutionSalesforce= new mongoose.Schema({
+// const mongoose = require('mongoose');
+// const solutionSalesforce= new mongoose.Schema({
     
-     img:{type:String,required:false},
-     imgtxt:{type:String,required:false},
-     imgonimg:{type:String,required:false},
-        title:{type:String,required:false},
-        para1:{type:String,required:false},
-        para2:{type:String,required:false},
+//      img:{type:String,required:false},
+//      imgtxt:{type:String,required:false},
+//      imgonimg:{type:String,required:false},
+//         title:{type:String,required:false},
+//         para1:{type:String,required:false},
+//         para2:{type:String,required:false},
 
-    sol:[{
-        img:{type:String,required:false},
-        imgtitle:{type:String,required:false},
-        desctitle:{type:String,required:false},
-        desc:{type: String,required:false}
-    }]
+//     sol:[{
+//         img:{type:String,required:false},
+//         imgtitle:{type:String,required:false},
+//         desctitle:{type:String,required:false},
+//         desc:{type: String,required:false}
+//     }]
    
 
-})
-exports.solutionSalesforce=mongoose.model("SolutionSalesforce",solutionSalesforce)
+// })
+// exports.solutionSalesforce=mongoose.model("SolutionSalesforce",solutionSalesforce)
 
 
--- mvc/controllers/salesforceController.js
+++ mvc/controllers/salesforceController.js
@@ -1,11 +1,11 @@
-const {solutionSalesforce}= require("../Models/solutionSalesforceModel")
+// const {solutionSalesforce}= require("../Models/solutionSalesforceModel")
 
-exports. getSalesforce= async(req,res)=>{
-    const data= await solutionSalesforce.find( )
-    res.send(data)
-}
+// exports. getSalesforce= async(req,res)=>{
+//     const data= await solutionSalesforce.find( )
+//     res.send(data)
+// }
 
-exports. postSalesforce= async(req,res)=>{
-    const data= await solutionSalesforce.create(req.body)
-    res.send(data)
-}
+// exports. postSalesforce= async(req,res)=>{
+//     const data= await solutionSalesforce.create(req.body)
+//     res.send(data)
+// }
